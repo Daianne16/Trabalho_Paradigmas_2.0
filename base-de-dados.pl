8e63d2bf9aad2631fcfb6362af861a2d8e89bde3
M4RUN <almeidamarcelia7@gmail.com>
2022-05-15 10:43:04 -0300

    Fazendo a consulta 4 retornar no formato correto

--- base-de-dados.pl
+++ base-de-dados.pl
@@ -12,10 +12,23 @@ quantidade(X, Res) :- findall(Qtd, gasto(_,X,_,Qtd,_,_,_), Quantidades), somaLis
 %Terceira regra
 valor_total(X, Res) :- findall(Val, gasto(_,X,_,_,_,_,Val), Valores), somaLista(Valores, Res).
 
-%Quarta regra ----- ERRADO
-comprado_em(D,X) :- findall(P , gasto(D,P,_,_,_,_,_) , X).
+%Quarta regra
+comprado_em(D,X) :- findall([Prod, Loja, Qtd] , gasto(D,Prod,Loja,Qtd,_,_,_) , X).
 
 %Quinta regra
 total_compras(X, Res) :- findall(Val, gasto(_,_,X,_,_,_,Val), Valores), somaLista(Valores, Res).
 
 %Sexta regra ----- NAO EXISTE
+
+gasto('11-03-2022','tijolo','joao',10.0,'unidade',25.0,840.0).
+gasto('10-01-2021','cimento','jose',50.0,'saco',24.0,650.0).
+gasto('20-05-2020','tijolo','joao',25.0,'unidade',22.0,560.0).
+gasto('11-03-2022','tijolo','joao',10.0,'unidade',25.0,840.0).
+gasto('10-01-2021','cimento','jose',50.0,'saco',24.0,650.0).
+gasto('20-05-2020','tijolo','joao',25.0,'unidade',22.0,560.0).
+gasto('11-03-2022','tijolo','joao',10.0,'unidade',25.0,840.0).
+gasto('10-01-2021','cimento','jose',50.0,'saco',24.0,650.0).
+gasto('20-05-2020','tijolo','joao',25.0,'unidade',22.0,560.0).
+gasto('11-03-2022','tijolo','joao',10.0,'unidade',25.0,840.0).
+gasto('10-01-2021','cimento','jose',50.0,'saco',24.0,650.0).
+gasto('20-05-2020','tijolo','joao',25.0,'unidade',22.0,560.0).
